c21c8aa70f3c1545297e616556429d1c7fb67765
Richard Sarvate <richsarvate@gmail.com>
2018-06-28 11:14:21 -0700

    updating the schedule

--- updateSchedule.rb
+++ updateSchedule.rb
@@ -1,6 +1,6 @@
 def print_comedian(name)
 
-  return 'document.write("<li class=\"comedian-tile\"><div class=\"headshot\"><img alt=\"'+name+'\" src=\"https://s3-us-west-1.amazonaws.com/comedyoakland/headshots/'+name.gsub(/\s+/, "")+'.jpg\" /><\/div><div><h1 class=\"comic-name\">'+name+'<\/h1><\/div><\/li>'
+  return '<li class=\"comedian-tile\"><div class=\"headshot\"><img alt=\"'+name+'\" src=\"https://s3-us-west-1.amazonaws.com/comedyoakland/headshots/'+name.gsub(/\s+/, "")+'.jpg\" /><\/div><div><h1 class=\"comic-name\">'+name+'<\/h1><\/div><\/li>'
 
 end
 
