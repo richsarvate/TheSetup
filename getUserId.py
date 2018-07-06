b6f19dc0648425a8e4eb2f3ef9d610fd86afee5c
Richard Sarvate <richsarvate@gmail.com>
2018-07-06 02:30:17 +0000

    updating the schedule

--- getUserId.py
+++
@@ -1,6 +0,0 @@
-from eventbrite import Eventbrite
-eventbrite = Eventbrite('XZJ5OCCSCTM3XJL5RNMW');
-
-user = eventbrite.get_user()  # Not passing an argument returns yourself
-print(user['id'])
-print(user['name'])
